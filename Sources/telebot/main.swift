e1ece9f87e2252c4bfd6bc72cb0037c0f595b253
Dmitriy Osipov <dosipov@azbukavkusa.ru>
2018-11-13 17:10:21 +0300

    try to process youtube link

--- Sources/telebot/main.swift
+++ Sources/telebot/main.swift
@@ -1,7 +1,7 @@
 import Telegrammer
 import Foundation
 
-// youtube-dl -i --extract-audio --audio-format mp3 --audio-quality 0 https://www.youtube.com/watch\?v\=IWTvgZVWeB4
+// youtube-dl -i --extract-audio --audio-format mp3 --audio-quality 0 -o "~/YouTubeFiles/%(title)s-%(id)s.%(ext)s" https://www.youtube.com/watch\?v\=IWTvgZVWeB4
 
 var token: Token = ""
 
@@ -18,11 +18,15 @@ do {
 let bot = try! Bot(token: token)
 
 let linkhandler = MessageHandler { (update, _) in
-    guard let message = update.message, let user = message.from else { return }
+    guard let message = update.message, let messageText = update.message?.text else { return }
+    
+    guard let validURL = URL(string: messageText) else { try! message.reply(text: "Кажеться, ты прислал мне не ссылку на YouTube!", from: bot); return }
+    
+    let youtubedlCommand = "youtube-dl -i --extract-audio --audio-format mp3 --audio-quality 0 -o \"~/YouTubeFiles/%(title)s-%(id)s.%(ext)s\" \(validURL)"
     
     let task = Process()
     task.launchPath = "/usr/bin/env"
-    task.arguments = ["pwd"]
+    task.arguments = [youtubedlCommand]
     
     let pipe = Pipe()
     task.standardOutput = pipe
@@ -30,8 +34,6 @@ let linkhandler = MessageHandler { (update, _) in
     
     let data = pipe.fileHandleForReading.readDataToEndOfFile()
     let output = NSString(data: data, encoding: String.Encoding.utf8.rawValue)
-    
-    print(output!)
     
     try! message.reply(text: "\(output!)", from: bot)
 }
