a612725758a2fb7e18ff3d979613bd974313cf5c
Dmitriy Osipov <dosipov@azbukavkusa.ru>
2018-11-13 20:55:45 +0300

    bug

--- Sources/telebot/main.swift
+++ Sources/telebot/main.swift
@@ -24,18 +24,18 @@ class YouTubeBot {
         let linkhandler = MessageHandler { (update, _) in
             guard let message = update.message, let messageText = update.message?.text else { return }
             
-            guard let validURL = URL(string: messageText) else { try! message.reply(text: "Кажеться, ты прислал мне не ссылку на YouTube!", from: bot); return }
-            
-            let _ = shell("/usr/local/bin/youtube-dl", "-i", "--extract-audio", "--audio-format", "mp3", "--audio-quality", "0", "-o", "/root/YouTubeFiles/%(title)s-%(id)s.%(ext)s", validURL.absoluteString)
-            
             var youTubePath = FileManager.default.homeDirectory(forUser: "root")!
             youTubePath.appendPathComponent("YouTubeFiles/")
             
             let fileList = try! FileManager.default.contentsOfDirectory(atPath: youTubePath.path)
-            
             for file in fileList {
                 try! FileManager.default.removeItem(atPath: youTubePath.appendingPathComponent(file).path)
             }
+            
+            guard let validURL = URL(string: messageText) else { try! message.reply(text: "Кажеться, ты прислал мне не ссылку на YouTube!", from: bot); return }
+            
+            let _ = shell("/usr/local/bin/youtube-dl", "-i", "--extract-audio", "--audio-format", "mp3", "--audio-quality", "0", "-o", "/root/YouTubeFiles/%(title)s-%(id)s.%(ext)s", validURL.absoluteString)
+            
             
             let needFileUrl = youTubePath.appendingPathComponent(fileList.first!)
             if let data = try? Data(contentsOf: needFileUrl) {
